0f5b1834d94209121aa7ad7dbdf5b669f4007393
omicrion782 <litvat@inbox.ru>
2022-09-27 18:47:28 +0300

    start

---
+++ src/js/files/script.js
@@ -0,0 +1,28 @@
+
+
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+
+
+
+
+
+
+
+window.onload = function () {
+
+setTimeout(function(){
+    console.log('хуй')
+}, 3000)
+    
+
+
+
+
+
+}
+
+
