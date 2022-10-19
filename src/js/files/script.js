6181beb3f8e31e9a7967b73765d739c43e28f294
omicrion782 <litvat@inbox.ru>
2022-10-19 17:13:35 +0300

    scroll

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -14,15 +14,14 @@ if (isMobile.any()) {
     document.body.classList.add("_pc");
 }
 
+
+////// DOCUMENT CLICK ACTION //////
+
 document.addEventListener('click', documentAction)
-
 function documentAction (e) {
-
     const targetElement = e.target;
 
-
-
-////// MENU-BURGER //////
+////// MENU-BURGER
     if (window.innerWidth < 768) {
         if (targetElement.closest(".icon-menu")) {
             targetElement.closest(".icon-menu").classList.toggle('_active');
@@ -31,7 +30,7 @@ function documentAction (e) {
     }
 
 
-////// NAVIGATION //////
+////// NAVIGATION
 
     if (targetElement.closest('.menu__list')){
         
@@ -79,19 +78,15 @@ function documentAction (e) {
         e.preventDefault();
     }
 
-
-
-
-
-
 }
+
+
+
 
 
 ////// FORM //////
 
     const contactForm = document.forms.contactForm;
-    
-
     contactForm.addEventListener("submit", sendMail);
     
     async function sendMail (e) {
@@ -100,7 +95,6 @@ function documentAction (e) {
         let error = formValidate(contactForm)
 
         let formData = new FormData(contactForm)
-        console.log(formData);
 
         if (error === 0) {
             contactForm.classList.add('_sending');
@@ -110,7 +104,6 @@ function documentAction (e) {
             });
             if (response.ok){
                 let result = await response.json();
-                console.log(result.message);
                 // contactForm.reset();
                 contactForm.classList.remove('_sending');
             } else {
@@ -119,14 +112,6 @@ function documentAction (e) {
             }
         } else {
         }
-
-
-
-
-
-
-
-
     }
 
     function formValidate (form) {
@@ -177,6 +162,33 @@ function formRemoveError (input) {
 
 
 
+////// HEADER //////
+
+
+
+////// INTERSECTION //////
+
+////// HEADER
+const headerTrigger = document.querySelector('.header__intersection-trigger');
+const headerElement = document.querySelector('.header__wrapper');
+
+let optionsHeader = {
+    rootMargin: '0px',
+    threshold: 1
+}
+
+
+const interCb = function (entries, observer) {
+	if(entries[0].isIntersecting && window.innerWidth > 768 ){
+        if (headerElement.classList.contains('_scroll')) {headerElement.classList.remove('_scroll')}
+    } else {
+        document.querySelector('.header__wrapper').classList.add("_scroll")
+    }
+}
+
+const headerObserver = new IntersectionObserver(interCb , optionsHeader);
+
+headerObserver.observe(headerTrigger);
 
 
 
@@ -185,21 +197,71 @@ function formRemoveError (input) {
 
 
 
+////// WINDOW RESIZING ACTION //////
 
 
 
 
+window.onresize = resizeActions;
+    function resizeActions (e) {
+
+    setOptionsHeaderMarks();
+
+}
 
 
 
 
+////// HEADER MARKS добавить в обработчик resize позже
 
+// установка опций для наблюдателя с учётом ширины экрана выполняется в resizeActions
 
+var optionsHeaderMarks = {}
+// настройки срабатывания intersection observer
+function setOptionsHeaderMarks (e) {
+        if (window.innerWidth < 768) { //mobile
+            optionsHeaderMarks = {
+                rootMargin: "-24% 0px -74% 1px",
+                threshold: 0.001
+            }
+        }
+        if (window.innerWidth < 992 && window.innerWidth > 768) { //tablet
+            optionsHeaderMarks = {
+                rootMargin: "-24% 0px -73% 2px",
+                threshold: 0.01
+            }
+        }
+        if (window.innerWidth > 992) { //pc
+            optionsHeaderMarks = {
+                rootMargin: "-25% 0px -70% 3px",
+                threshold: 0.01
+            }
+        }
+        // console.log(optionsHeaderMarks);
+}
 
+setOptionsHeaderMarks();
 
+const interHeaderMarksCb = function (entries, observer) {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            navItems.forEach(item => {
+                item.classList.remove('nav-intersecting')
+            })
+            let index = pageItems.indexOf(entry.target);
+            navItems[index].classList.add('nav-intersecting')
+        }
+    });
+}
 
+const headerMarksObserver = new IntersectionObserver(interHeaderMarksCb , optionsHeaderMarks);
 
+const pageItems = Array.from(document.querySelectorAll('.nav-intersection-item'));
+const navItems = Array.from(document.querySelector('.menu__list').children)
 
+pageItems.forEach((item)=>{
+    headerMarksObserver.observe(item);
+})
 
 
 
